79fac7b119f77e0a44ade68a19efe4ac9d32ac05
TanumaHideki <tanuma@ruby.plala.or.jp>
2025-07-21 11:49:41 +0900

    audio distortion

--- c5g/loader/relm_audio_adpcm.py
+++ c5g/loader/relm_audio_adpcm.py
@@ -27,7 +27,15 @@ def compress(frames, la=6):
                 shift = 0
             history = history[m::4, :]
             history[:, -1] -= np.abs(history[:, 0] - frames[i - la])
+        pred = (
+            history[:, la - 6]
+            - history[:, la - 5] * 2
+            + history[:, la - 4]
+            - history[:, la - 2]
+            + history[:, la - 1] * 2
+        )
         history[:, : la - 1] = history[:, 1:la]
+        history[:, la - 1] = pred
         history[:, la] = (np.abs(history[:, la]) + 1) >> 1
         h = history.copy()
         h[:, la] *= -1
@@ -61,7 +69,7 @@ if filename:
     else:
         audio_data = AudioSegment.from_file(filename, format=filename[-3:])
         audio_data = audio_data.set_channels(1)  # Convert to mono if not already
-        audio_data = audio_data.set_frame_rate(48000)  # Set to 48kHz
+        audio_data = audio_data.set_frame_rate(32000)  # Set to 32kHz
         samples = audio_data.get_array_of_samples()
         if len(samples) > 90000 * 16:
             samples = samples[: 90000 * 16]
@@ -89,7 +97,7 @@ if filename:
                 6, 0x61
             ),  # POWER MANAGEMENT: PWROFF=0, CLKOUT=1, OSC=1, Out=0, DAC=0, ADC=0, MIC=0
             audio.i2c(7, 0x42),  # DIGITAL AUDIO I/F: MS=1, WL=00
-            audio.i2c(8, 0x02),  # SAMPLING RATE: SR=0000, BOSR=1 (48kHz)
+            audio.i2c(8, 0x1A),  # SAMPLING RATE: SR=0110, BOSR=1 (32kHz)
             audio.i2c(16, 0x1FB),  # ALC CONTROL 1: ALCSEL=11
             audio.i2c(17, 0x32),  # ALC CONTROL 2
             audio.i2c(18, 0x00),  # NOISE GATE
@@ -105,7 +113,12 @@ if filename:
             Do()[
                 If(fifo1.IsEmpty())[
                     If(reset != 0)[
-                        sample := Int(0),
+                        pred0 := Int(0),
+                        pred1 := Int(0),
+                        pred2 := Int(0),
+                        pred3 := Int(0),
+                        pred4 := Int(0),
+                        pred5 := Int(0),
                         delta := Int(1),
                         reset(0),
                     ],
@@ -114,15 +127,29 @@ if filename:
                 code := Int(fifo1.Pop()),
                 i := Int(16),
                 Do()[
+                    pred6 := Int(),
                     audio.Push(
-                        sample(
-                            delta(((code & 3) | 1) * ((delta + 1) >> 1))
-                            * ((code & 1) * 2 - 1)
-                            + sample
+                        (
+                            pred6(
+                                pred0
+                                - pred1 * 2
+                                + pred2
+                                - pred4
+                                + pred5 * 2
+                                + delta(((code & 3) | 1) * ((delta + 1) >> 1))
+                                * ((code & 1) * 2 - 1)
+                            )
+                            & 0xFFFF
                         )
-                        & 0xFFFF * 0x10001
+                        * 0x10001
                     ),
                     code(code >> 2),
+                    pred0(pred1),
+                    pred1(pred2),
+                    pred2(pred3),
+                    pred3(pred4),
+                    pred4(pred5),
+                    pred5(pred6),
                 ].While(i(i - 1) != 0),
             ],
         ]
